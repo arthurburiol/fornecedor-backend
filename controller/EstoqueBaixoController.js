1a5ff9f0f16a88d059043b88909aeb5f0c77b8e7
Arthur Buriol <arthur.bcontas@gmail.com>
2025-07-01 20:11:15 -0300

    FINAL

--- controller/EstoqueBaixoController.js
+++ controller/EstoqueBaixoController.js
@@ -1,9 +1,9 @@
-import ProdutosEstoqueBaixoView from '../models/ProdutosEstoqueBaixoView.js';
+import ProdutosEstoqueBaixoView from "../model/EstoqueBaixoModel.js";
 
 const listarEstoqueBaixo = async (req, res) => {
   try {
     const dados = await ProdutosEstoqueBaixoView.findAll();
-    res.json(dados); // JSON com os produtos de estoque baixo
+    res.json(dados);
   } catch (error) {
     console.error('Erro ao buscar produtos com estoque baixo:', error);
     res.status(500).json({ erro: 'Erro ao buscar produtos com estoque baixo' });
